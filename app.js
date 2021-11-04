1de1eebb5175362f164bc0c743c17c41ba48e1cd
Brant Han <brant.han@gmail.com>
2021-11-04 10:16:36 -0700

    counter complete

---
+++ app.js
@@ -0,0 +1,34 @@
+//set initial count
+let count = 0;
+
+// select value and buttons
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn')
+
+console.log(btns);
+
+btns.forEach(function(btn){
+    // console.log(btn);
+    btn.addEventListener("click", function (e){
+       const styles = e.currentTarget.classList;
+       if (styles.contains("decrease")) {
+         count--;
+         //    console.log(count);
+       } else if (styles.contains("increase")) {
+         count++;
+         //    console.log(count);
+       } else {
+           count = 0;
+       }
+       if (count > 0) {
+         value.style.color = "green";
+       } else if (count < 0) {
+         value.style.color = "red";
+       } else {
+         value.style.color = "black";
+       }
+    
+       value.textContent = count;
+    });
+    
+});
